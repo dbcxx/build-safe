8b1f012dcd83bd37fcc66072d79e665b3474e9b5
Dolapo <40644301+dbcxx@users.noreply.github.com>
2023-02-07 19:44:32 +0100

    fisrt stage init

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -5,29 +5,31 @@ import { TypeAnimation } from "react-type-animation";
 const Header = () => {
   return (
     <header id="welcome-section">
-      <div className="forest" />
-
       <div className="container">
         <div className="typing">
-        <TypeAnimation
-          // Same String at the start will only be typed once, initially
-          sequence={[
-            "Ensure buildings are safe for habiting",
-            1000,
-            "Ensure buildings are safe for Sex",
-            2000,
-            "Ensure buildings are safe for Ageing",
-            3000,
-            "Ensure buildings are safe for Death",
-            4000,
-          ]}
-          speed={20} // Custom Speed from 1-99 - Default Speed: 40
-          style={{ }}
-          wrapper="span" // Animation will be rendered as a <span>
-          repeat={Infinity} // Repeat this Animation Sequence infinitely
-        />
+          <TypeAnimation
+            // Same String at the start will only be typed once, initially
+            sequence={[
+              "Ensure buildings are safe for habiting",
+              1000,
+              "Ensure buildings are safe for Sex",
+              2000,
+              "Ensure buildings are safe for Ageing",
+              3000,
+              "Ensure buildings are safe for Death",
+              2000,
+            ]}
+            speed={20} // Custom Speed from 1-99 - Default Speed: 40
+            style={{}}
+            wrapper="span" // Animation will be rendered as a <span>
+            repeat={Infinity} // Repeat this Animation Sequence infinitely
+          />
         </div>
-        
+        <div className=" text-p">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud
+        </div>
       </div>
     </header>
   );
