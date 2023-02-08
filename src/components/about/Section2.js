c2a50f1a65ef5e5a3783fbb9269ec7475ff2ad8b
Dolapo <40644301+dbcxx@users.noreply.github.com>
2023-02-08 20:11:03 +0100

    changes

--- src/components/about/Section2.js
+++ src/components/about/Section2.js
@@ -10,14 +10,13 @@ const Section2 = () => {
         <div className="heading">
           <h2 className="section-heading">about us</h2>
           <p className="sub-heading">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum
-          </p>
+          BuildSure Limited is an insurance technology SPV and is the primary developer and promoter of the BuildSure 
+project and its technologies.
+BuildSure is a subsidiary of Baye Capital, a venture studio with a track record of building and implementing 
+large scale national technology infrastructure that form the building blocks of smart cities. BuildSure has a 
+sister company implementing a 10 year project between the Federal Road Safety Commission, State Traffic 
+enforcement agencies and the Nigerian Insurers Association to ensure adequate vehicle insurance penetration 
+and develop the official risk rating system of the industry. </p>
         </div>
         <div className="row-container">
           <div className="row">
--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,53 +1,110 @@
 import React from "react";
-import './contact.css';
+import "./contact.css";
 
 const Contact = () => {
-
-    return (
-        <>
-        <section class = "contact" id = "contact">
-        <div class = "container">
-          <div class = "title">
-            <h2 class = "wow animate__animated animate__bounceIn animate__fast">Contact Us</h2>
-            <p class = "text">How to contact us</p>
+  return (
+    <>
+      <section class="contact" id="contact">
+        <div class="container-1">
+          <div className="contact-first">
+          <div class="title">
+              <h2 class="mob-align wow animate__animated animate__bounceIn animate__fast">
+                Quick Links 
+              </h2>
+            </div>
+            <div className="contact-first-links">
+              <ul className="first-space">
+                <li>
+                  <a href="/">About us </a>
+                </li>
+                <li>
+                  <a href="/">Career </a>
+                </li>
+                <li>
+                  <a href="/">Policy </a>
+                </li>
+                <li>
+                  <a href="/">location </a>
+                </li>
+                <li>
+                  <a href="/">Login </a>
+                </li>
+                <li>
+                  <a href="/">Create Account </a>
+                </li>
+              </ul>
+            </div>
           </div>
-  
-          <div class = "row wow animate__animated animate__fadeInUp animate__fast">
-            <div class = "contact-left">
-              <h2>Social media</h2>
-                 <div class = "footer-links">
-                    <a href = "https://wa.me/" class = "center">
-                      <i class = "fab fa-whatsapp"></i>
-                    </a>
-                    <a href = "https://t.me/joinchat/" class = "center">
-                      <i class = "fab fa-telegram"></i>
-                    </a>
-                    <a href = "tel:+2340000000000" class = "center">
-                      <i class="fas fa-phone-alt"></i>
-                    </a>
-                     <a href = "www.twitter.com" class = "center">
-                      <i class = "fab fa-twitter"></i>
-                    </a>
+
+
+          <div className="contact-middle">
+          <div class="title">
+              <h2 class=" mob-align  wow animate__animated animate__bounceIn animate__fast">
+                Features
+              </h2>
+              
+            </div>
+            <div className="contact-first-links">
+              <ul className="first-space">
+                <li>
+                  <a href="/">Agencies </a>
+                </li>
+                <li>
+                  <a href="/">Experts </a>
+                </li>
+                <li>
+                  <a href="/">Investor Relations </a>
+                </li>
+                <li>
+                  <a href="/">Terms of Service  </a>
+                </li>
+                {/* <li>
+                  <a href="/">Login </a>
+                </li>
+                <li>
+                  <a href="/">Create Account </a>
+                </li> */}
+              </ul>
+            </div>
+          </div>
+          <div className="contact-last">
+            <div class="title">
+              <h2 class="  wow animate__animated animate__bounceIn animate__fast">
+                Contact Us
+              </h2>
+              
+            </div>
+            <div class=" wow animate__animated animate__fadeInUp animate__fast">
+              <div class="contact-left">
+                
+                <div class="footer-links">
+                  <a href="https://wa.me/" class="center">
+                    <i class="fab fa-whatsapp"></i>
                     
-                  </div>
+                  </a>
+                  <a href="https://t.me/joinchat/" class="center">
+                    <i class="fab fa-telegram"></i>
+                  </a>
+                  <a href="tel:+2340000000000" class="center">
+                    <i class="fas fa-phone-alt"></i>
+                  </a>
+                  <a href="www.twitter.com" class="center">
+                    <i class="fab fa-twitter"></i>
+                  </a>
                 </div>
-               
-  
-            
+              </div>
+            </div>
           </div>
         </div>
       </section>
-      
-  
-     
-      <footer class = "footer center">
-        <div class = "container">
-          <p class = "text">Copyright &copy; 2023 - BuildSafe</p>
-       </div>
-      </footer>
-      </>
-    );
 
+      <footer class="footer center">
+        <div class="container">
+          <p class="text">Copyright &copy; 2023 All rights reserved || BuildSafe</p>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Contact;
